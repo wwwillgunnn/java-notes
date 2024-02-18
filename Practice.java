e0102683dcbafeb8ecc311aebc28f7ca7f6fe7b2
Will <124865059+wwwillgunnn@users.noreply.github.com>
2024-02-18 16:34:02 +1100

    Update Practice.java

--- Practice.java
+++ Practice.java
@@ -1,7 +1,5 @@
-// class
-public class Practice {
-    // main method
-    public static void main(String[] args){
+public class Practice { // class
+    public static void main(String[] args){  // The main method executes the tasks of the class
         // All data types must be declared before their name
         // Concatenate things with a plus
         String user = "User";
