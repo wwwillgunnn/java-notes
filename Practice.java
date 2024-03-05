4c4ecc07a66844de4fac4693abc4152d8b7ec0ba
Skwerks <133098215+Skwerks@users.noreply.github.com>
2024-03-06 10:27:52 +1100

    Update Practice.java

--- Practice.java
+++ Practice.java
@@ -606,3 +606,37 @@ public class JavaCourse {
         return a + b + c + d;
 }
 }
+
+//printf
+public class JavaCourse {
+
+    public static void main(String[] args) {
+
+        //printf() = an optional method to control, format, and display text to the console windows
+        //           two arguments = format string + (object/variable/value)
+        //           % [flags] [precision] [width] [conversion-character]
+        // for number formatting use %d
+        //for decimal number formatting and double %f
+        //for boolean formatting ( %b or %B)
+        // for char formatting (%c or %C)
+        //for string formatting (%s and %S)
+
+        //[width]
+        //minimum number of characters to be written as output 
+        //System.out.printf("hello %10s", myString);
+
+        //[Precision]
+        //sets number of digits of precision 
+        //System.out.printf("You have this much money %.1f", myDouble)
+
+        //[flags]
+        //adds an effect to output based on the flag added to format specifier 
+        // - : left-justify
+        // + : output a plus ( + ) or minus ( - ) sign for a numeric value 
+        // 0 : numeric values are zero-padded
+        // , : comma grouping seperator if numbers > 1000
+
+     System.out.printf(" %d This is a format string ",123);
+
+}
+}
