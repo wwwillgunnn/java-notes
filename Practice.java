595c8873eff098e19b5fdd1c9925ca5079489db8
Skwerks <133098215+Skwerks@users.noreply.github.com>
2024-03-05 19:54:02 +1100

    Update Practice.java

--- Practice.java
+++ Practice.java
@@ -580,3 +580,29 @@ public class JavaCourse {
         System.out.println("Hello");
     }
 }
+
+
+// overloaded methods
+public class JavaCourse {
+
+    public static void main(String[] args) {
+        
+        // overloaded methods = methods that share the same name but have different parameters
+        //                      method name + parameters = method signature  
+        
+        int x = add(1,2, 3, 4);
+        System.out.println(x);
+    }
+    static int add(int a, int b) {
+        System.out.println("This is overloaded method #1");
+        return a + b;
+    }
+    static int add(int a, int b, int c) {
+        System.out.println("This is overloaded method #2");
+        return a + b + c;
+    }
+    static int add(int a, int b, int c, int d) {
+        System.out.println("This is overloaded method #3");
+        return a + b + c + d;
+}
+}
